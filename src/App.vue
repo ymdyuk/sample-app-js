643243c3606c0c34b777055b69b160931726f1d4
ymdyuk <h27e67f897k6qyx@softbank.ne.jp>
2021-01-18 02:23:08 +0900

    初回コミット(~step9)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+    <footer />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import FooterComponent from '@/components/Footer.vue';
+
+export default defineComponent({
+  components: {
+    Footer: FooterComponent,
+  },
+});
+</script>
